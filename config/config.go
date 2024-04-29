751a9a9d9444e313428ed82083eed96cb72bb621
Karn  Kraiket <106549243+Gitong23@users.noreply.github.com>
2024-04-29 10:21:49 +0000

    init config yaml

--- config/config.go
+++ config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import "time"
+
+type (
+	Config struct {
+	}
+
+	Server struct {
+		Port           int           `mapstructure:"port" validate:"required"`
+		AllowedOrigins []string      `mapstructure:"allowOrigins" validate:"required"`
+		BodyLimit      string        `mapstructure:"bodyLimit" validate:"required"`
+		TimeOut        time.Duration `mapstructure:"timeout" validate:"required"`
+	}
+
+	OAuth struct {
+	}
+
+	State struct {
+	}
+
+	Database struct {
+	}
+)
