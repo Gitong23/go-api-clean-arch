20e564ab0cea2d62569acb429ee8350a7057e2a0
Karn  Kraiket <106549243+Gitong23@users.noreply.github.com>
2024-04-29 09:29:41 +0000

    change name of entities

--- pkg/itemShop/controller/itemShopControllerImpl.go
+++ pkg/itemShop/controller/itemShopControllerImpl.go
@@ -7,3 +7,7 @@ import (
 type itemShopControllerImpl struct {
 	itemShopService _itemShopService.ItemShopService
 }
+
+func NewItemShopController(itemShopService _itemShopService.ItemShopService) ItemShopController {
+	return &itemShopControllerImpl{itemShopService}
+}
