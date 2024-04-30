ae21ea34c422091bbccd51c77413ce669d2c05a4
Karn  Kraiket <106549243+Gitong23@users.noreply.github.com>
2024-04-30 07:18:24 +0000

    init echo server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/Gitong23/go-api-clean-arch/config"
+	"github.com/Gitong23/go-api-clean-arch/databases"
+	"github.com/Gitong23/go-api-clean-arch/server"
+)
+
 func main() {
-	println("Hello, World!")
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
 }
