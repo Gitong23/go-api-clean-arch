7e9c8c613f198ac8dd8c87ac275137b9b26a7365
Karn  Kraiket <106549243+Gitong23@users.noreply.github.com>
2024-04-30 09:07:48 +0000

    init built in middleware

--- server/server.go
+++ server/server.go
@@ -1,12 +1,20 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/Gitong23/go-api-clean-arch/config"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	// "github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
@@ -38,7 +46,25 @@ func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
 }
 
 func (s *echoServer) Start() {
-	s.app.GET("v1/health", s.healthCheck)
+
+	corsMiddleware := getCORSMiddleware(s.conf.Server.AllowOrigins)
+	bodyLimitMiddleware := getBodyLimitMiddleware(s.conf.Server.BodyLimit)
+	timeoutMiddleware := getTimeOutMiddleware(s.conf.Server.Timeout)
+
+	s.app.Use(middleware.Recover())
+	s.app.Use(middleware.Logger())
+	s.app.Use(corsMiddleware)
+	s.app.Use(bodyLimitMiddleware)
+	s.app.Use(timeoutMiddleware)
+
+	s.app.GET("/v1/health", s.healthCheck)
+	// s.app.GET("/v1/panic", func(c echo.Context) error {
+	// 	panic("Panic!")
+	// })
+
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
+	go s.gracfullyShutdown(quitCh)
 	s.httpListening()
 
 }
@@ -51,6 +77,42 @@ func (s *echoServer) httpListening() {
 	}
 }
 
+func (s *echoServer) gracfullyShutdown(quitCh chan os.Signal) {
+	ctx := context.Background()
+
+	<-quitCh
+	s.app.Logger.Info("Shutting down server...")
+
+	if err := s.app.Shutdown(ctx); err != nil {
+		s.app.Logger.Fatalf("Error: %s", err.Error())
+	}
+}
+
 func (s *echoServer) healthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "OK Krub")
+}
+
+// func getLoggerMiddleware() echo.MiddlewareFunc {
+// 	return middleware.Logger()
+// }
+
+func getTimeOutMiddleware(timeout time.Duration) echo.MiddlewareFunc {
+	return middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper:      middleware.DefaultSkipper,
+		ErrorMessage: "Request Timeout",
+		Timeout:      timeout * time.Second,
+	})
+}
+
+func getCORSMiddleware(allowOrigins []string) echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: allowOrigins,
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	})
+}
+
+func getBodyLimitMiddleware(bodyLimit string) echo.MiddlewareFunc {
+	return middleware.BodyLimit(bodyLimit)
 }
