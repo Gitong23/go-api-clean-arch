bfb43ebd1757f3417a24b64d99d9c69444bef773
Karn.K <karn.k@haupcar.com>
2024-05-08 17:56:55 +0700

    auth middleware

---
+++ server/authorizingMiddleware.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"github.com/Gitong23/go-api-clean-arch/config"
+	_oauth2Controller "github.com/Gitong23/go-api-clean-arch/pkg/oauth2/controller"
+	"github.com/labstack/echo/v4"
+)
+
+type authorizingMiddleware struct {
+	oauthController _oauth2Controller.OAuth2Controller
+	oauth2Conf      *config.OAuth2
+	logger          echo.Logger
+}
+
+// func (m *authorizingMiddleware) PlayerAuthorizing(next echo.HandlerFunc) echo.HandlerFunc {
+// 	return func(pctx echo.Context) error {
+// 		return m.oauthController.P
+// 	}
+// }
